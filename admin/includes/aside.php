4b2a52c20c77a0d8450119aa731330c0eb451403
unknown <tuyizereabdul01@gmail.com>
2023-08-16 17:19:48 +0200

    first commit

---
+++ admin/includes/aside.php
@@ -0,0 +1,65 @@
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="index.php">
+      <i class="bi bi-speedometer2"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link " href="customers.php">
+      <i class="bi bi-people-fill"></i>
+      <span>Customers</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+  <li class="nav-item">
+    <a class="nav-link " href="venders.php">
+      <i class="bi bi-minecart"></i>
+      <span>Suppliers</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link " href="product.php">
+      <i class="bi bi-dropbox"></i>
+      <span>Product</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+
+
+  <li class="nav-item">
+    <a class="nav-link " href="sales.php">
+      <i class="bi bi-bar-chart-line-fill"></i>
+      <span>Sales</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link " href="day_report.php">
+      <i class="bi bi-card-checklist"></i>
+      <span>Dayily report</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link " href="month_report.php">
+      <i class="bi bi-card-list"></i>
+      <span>Monthly report</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+  <li class="nav-item">
+    <a class="nav-link " href="users.php">
+      <i class="bi bi-person"></i>
+      <span>Users</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  
+
+</ul>
+
+</aside><!-- End Sidebar-->
